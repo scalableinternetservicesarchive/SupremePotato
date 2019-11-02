047a593eb56da46c9b6b714e5dbb7038bc2b48d0
sarawang <369278452@qq.com>
2019-11-01 20:14:49 -0700

    add order and trade process

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -12,45 +12,6 @@ class TradesController < ApplicationController
   def show
   end
 
-  # GET /trades/new
-  def new
-    @trade = Trade.new
-  end
-
-  # GET /trades/1/edit
-  def edit
-  end
-
-  # POST /trades
-  # POST /trades.json
-  def create
-    @trade = Trade.new(trade_params)
-
-    respond_to do |format|
-      if @trade.save
-        format.html { redirect_to @trade, notice: 'Trade was successfully created.' }
-        format.json { render :show, status: :created, location: @trade }
-      else
-        format.html { render :new }
-        format.json { render json: @trade.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /trades/1
-  # PATCH/PUT /trades/1.json
-  def update
-    respond_to do |format|
-      if @trade.update(trade_params)
-        format.html { redirect_to @trade, notice: 'Trade was successfully updated.' }
-        format.json { render :show, status: :ok, location: @trade }
-      else
-        format.html { render :edit }
-        format.json { render json: @trade.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /trades/1
   # DELETE /trades/1.json
   def destroy
@@ -65,10 +26,5 @@ class TradesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_trade
       @trade = Trade.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def trade_params
-      params.require(:trade).permit(:buy_id, :sell_id, :company_id, :price)
     end
 end
