4f8ac2afacdca7d67b4585d29c6148ff2f6d7872
Siddarth Senthilkumar <sidsenkumar11@gmail.com>
2019-11-16 15:05:17 -0800

    Add default price field for company based on trades or pending sell order from CEO

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,6 +4,17 @@ class Company < ApplicationRecord
     has_many :holdings
 
     def price
-        @price ||= self.trades.order('updated_at DESC').last.price
+        if @price.nil?
+            trade  = self.trades.order('updated_at DESC').last
+
+            if trade
+                @price = trade.price
+            else
+                # Get CEO's sale price
+                @price = self.orders.where(order_type: Order::SELL).order('updated_at DESC').first.price
+            end
+        end
+
+        @price
     end
 end
