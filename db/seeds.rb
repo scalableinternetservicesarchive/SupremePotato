ada60c0a38fb1f8520426c0af5a2beabb4dccbfa
Chen Z <chenzhu@ucsb.edu>
2019-11-18 10:41:54 -0800

    Merge in Sid's seed.rb style

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,42 +4,42 @@
 #Companies list
 #name, ticker, shares, starting_price
 companies_list = [
-  [ "Apple", 	"AAPL", 	20, 200],
-  [ "Google", 	"GOOGL", 	30, 110],
-  [ "Facebook", "FB", 		15, 100],
-  [ "Amazon", 	"AMZN", 	20, 150]
+  [ "Apple",    "AAPL",  20, 200],
+  [ "Google",   "GOOGL", 30, 110],
+  [ "Facebook", "FB",    15, 100],
+  [ "Amazon",   "AMZN",  20, 150],
 ]
 
 companies_list.each do |name, ticker, shares, price|
   #Crate companies entries
   company = Company.create(
-		  	name:		name, 
-		  	ticker:		ticker,  
-		  	shares:		shares
-	  	)
+    name:    name,
+    ticker:  ticker,
+    shares:  shares,
+  )
 
   #Create CEO users entries
   ceo = User.create(
-  			name: 		name + "-CEO", 
-  			balance: 	0
-  		)
+    name:    name + "-CEO",
+    balance: 1000000,
+  )
 
   #Crate User holdinng entries
   holdings = Holding.create(
-		  	user_id: 	ceo.id, 
-		  	company_id:	company.id, 
-		  	quantity: 	shares
-	  	)
+    user_id:    ceo.id,
+    company_id: company.id,
+    quantity:   shares,
+  )
 
   #Create #shares number of sales orders (because each order only allow one share atm)
   shares.times {
-  	Order.create(
-	  		price: 		price, 
-	  		company_id:	company.id, 
-	  		user_id:	ceo.id, 
-	  		status: 	Order::PENDING, 
-	  		order_type:	Order::SELL,
-  		)
+    Order.create(
+      price:      price,
+      company_id: company.id,
+      user_id:    ceo.id,
+      status:     Order::PENDING,
+      order_type: Order::SELL,
+    )
   }
 end
 
