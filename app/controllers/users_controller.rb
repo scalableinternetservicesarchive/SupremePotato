5c2beff24ccf6fbae57824fd1cab49111571d90f
Chen Z <chenzhu@ucsb.edu>
2019-11-20 22:01:17 -0800

    Make pagination consistent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
-    @users = User.all.paginate(:page => params[:page], :per_page => 15)
+    @users = User.all.order('id DESC').paginate(:page => params[:page], :per_page => 15)
   end
 
   def show
