b80df09be5ddce6d41ec42eed36125c3a22c60a5
Chen Z <chenzhu@ucsb.edu>
2019-11-24 20:26:40 -0800

    Updated Order Cache

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -4,15 +4,15 @@ class Trade < ApplicationRecord
     belongs_to :company
 
     def cached_buy_order
-		Rails.cache.fetch('order_id' + self.id.to_s) do 
-			Rails.logger.info '<<<CACHE NOT FOUND + DB CALL>>>' + 'buy_order_id' + self.buy_order_id.to_s
+		Rails.cache.fetch('order_id' + self.buy_order_id.to_s) do 
+			Rails.logger.info '<<<CACHE NOT FOUND + DB CALL>>>' + 'order_id' + self.buy_order_id.to_s
 			self.buy_order
 		end
 	end
 
 	def cached_sell_order
-		Rails.cache.fetch('order_id' + self.id.to_s) do 
-			Rails.logger.info '<<<CACHE NOT FOUND + DB CALL>>>' + 'sell_order_id' + self.sell_order_id.to_s
+		Rails.cache.fetch('order_id' + self.sell_order_id.to_s) do 
+			Rails.logger.info '<<<CACHE NOT FOUND + DB CALL>>>' + 'order_id' + self.sell_order_id.to_s
 			self.sell_order
 		end
 	end
