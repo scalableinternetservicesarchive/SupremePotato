70c6c0a71f844b8051cec16b96dda497db37ab87
Chen Z <chenzhu@ucsb.edu>
2019-12-03 11:29:57 -0800

    Delete 20191124212459_trades_foreign_keys.rb

--- db/migrate/20191124212459_trades_foreign_keys.rb
+++
@@ -1,6 +0,0 @@
-class TradesForeignKeys < ActiveRecord::Migration[5.2]
-  def change
-  	#add_foreign_key :trades, :orders, column: :buy_order_id, primary_key: "id"
-    #add_foreign_key :trades, :orders, column: :sell_order_id, primary_key: "id"
-  end
-end
