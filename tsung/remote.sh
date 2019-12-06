c44cec7ebc72f7ef4e77342f6dd4bfa8491e58b6
Kevin Burk <xavierholt@gmail.com>
2019-12-05 19:40:29 -0800

    More load testing bugfixes.

--- tsung/remote.sh
+++ tsung/remote.sh
@@ -4,4 +4,8 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 KEY="~/.ssh/SupremePotato.pem"
 
 scp -i "$KEY" "$DIR/orders.xml" "$DIR/userlist.csv" "ec2-user@$1:"
-ssh -i "$KEY" "ec2-user@$1" tsung -kf orders.xml start
+ssh -i "$KEY" "ec2-user@$1" <<EOF
+pkill epmd
+pkill beam.smp
+tsung -kf orders.xml start
+EOF
